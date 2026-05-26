292a2154ef0f5819070a4ebb2c3a075a06dd713c
feli-marmar <feliciamputri@gmail.com>
2026-05-26 22:28:33 +0700

    feat(voucher): add Voucher CRUD with model, migration, controller, and routes

--- a06-loyaltyapps/routes/web.php
+++ a06-loyaltyapps/routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\VoucherController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('posts', PostController::class);
+Route::resource('vouchers', VoucherController::class);
