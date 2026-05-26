bba6061b11884a0ca02357d85b2a895affce121d
vioshellina <agitaputri05@gmail.com>
2026-05-26 22:12:50 +0700

    feat(redeem): add redeems table migration

--- a06-loyaltyapps/routes/web.php
+++ a06-loyaltyapps/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RedeemController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/redeem', [RedeemController::class, 'create'])->name('redeem.create');
+    Route::post('/redeem', [RedeemController::class, 'store'])->name('redeem.store');
+    Route::get('/redeem/history', [RedeemController::class, 'history'])->name('redeem.history');
 });
