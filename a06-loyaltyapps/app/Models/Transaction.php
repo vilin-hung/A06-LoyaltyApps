0b21800557069f29dff82cd77cce71c46ea5e399
vilin-hung <vianlienra@gmail.com>
2026-05-27 17:14:51 +0700

    feat(transaction): add order and transaction history with service

--- a06-loyaltyapps/app/Models/Transaction.php
+++ a06-loyaltyapps/app/Models/Transaction.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = ['user_id', 'total_amount', 'points_earned'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    public function items() {
+        return $this->hasMany(TransactionItem::class);
+    }
 }
