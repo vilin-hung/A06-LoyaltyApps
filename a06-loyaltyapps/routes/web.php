0225a12ef74f234b3bb19298db9de97f8a9d78d8
FeliciaFrederica <feliciafrederica1974@gmail.com>
2026-05-27 18:44:07 +0700

    chore(routes): add code comments for readability

--- a06-loyaltyapps/routes/web.php
+++ a06-loyaltyapps/routes/web.php
@@ -7,7 +7,18 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\VoucherController;
 use App\Http\Controllers\RedeemController;
 
-Route::resource('products', ProductController::class);
+// Route Product - User biasa hanya bisa melihat daftar product dan detail product
+Route::resource('products', ProductController::class)
+    ->only(['index', 'show']);
+
+// Route Product (Admin) - CRUD product hanya bisa dilakukan oleh user yang sudah login dan role admin
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('products', ProductController::class)
+        ->except(['index', 'show']);
+});
+
+// Route Review - User dapat mengedit review miliknya,
+// sedangkan admin memiliki hak untuk menghapus review (spam, SARA, dll)
 Route::resource('reviews', ReviewController::class);
 Route::resource('vouchers', VoucherController::class);
 Route::middleware('auth')->group(function () {
