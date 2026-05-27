fc3b69a2c32f1c01410cae6414a1f97ca401b6e4
vilin-hung <vianlienra@gmail.com>
2026-05-27 15:16:59 +0700

    feat(auth): add admin middleware and protect product routes

--- a06-loyaltyapps/routes/web.php
+++ a06-loyaltyapps/routes/web.php
@@ -1,23 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 
-//Route::middleware(['auth', 'admin'])->group(function () {
-    Route::resource('products', ProductController::class);
-//});
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\VoucherController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RedeemController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VoucherController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('posts', PostController::class);
+Route::resource('products', ProductController::class);
+Route::resource('transactions', TransactionController::class);
 Route::resource('vouchers', VoucherController::class);
-Route::middleware('auth')->group(function () {
-    Route::get('/redeem', [RedeemController::class, 'create'])->name('redeem.create');
-    Route::post('/redeem', [RedeemController::class, 'store'])->name('redeem.store');
-    Route::get('/redeem/history', [RedeemController::class, 'history'])->name('redeem.history');
-});
+// Route::middleware('auth')->group(function () {
+// });
