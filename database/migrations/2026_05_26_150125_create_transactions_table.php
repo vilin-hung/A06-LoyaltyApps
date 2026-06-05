941002d56dfc19e0089c9d8d131f3ac5d81ad2ed
vilin-hung <vianlienra@gmail.com>
2026-06-05 22:49:46 +0700

    feat(backend): update transaction logic to support dynamic cart item selections and vouchers

--- database/migrations/2026_05_26_150125_create_transactions_table.php
+++ database/migrations/2026_05_26_150125_create_transactions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('voucher_id')->nullable()->constrained()->onDelete('set null'); 
             $table->decimal('total_amount', 10, 2);
             $table->integer('points_earned')->default(0);
             $table->timestamps();
