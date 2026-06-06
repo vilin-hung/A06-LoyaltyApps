106bca624461869586934ee177cf2b19baf715c8
vioshellina <agitaputri05@gmail.com>
2026-06-06 18:02:46 +0700

    feat(redeem): improve redeem views and voucher relation part kesekian

--- a06-loyaltyapps/app/Models/User.php
+++ a06-loyaltyapps/app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Redeem::class);
     }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
 }
