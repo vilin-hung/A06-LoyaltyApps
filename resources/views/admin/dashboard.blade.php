9f70aa37add57fea3497abcfa6e514c6ac1c5e52
vilin-hung <vianlienra@gmail.com>
2026-06-07 21:09:57 +0700

    refactor(views): edit home layout and rename transaction view files

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,11 +12,12 @@
     <h1>Dashboard Admin</h1>
     
     <ul>
+        <li><a href="{{ route('news.index') }}">Daftar Berita</a></li>
+        <li><a href="{{ route('memberships.index') }}">Daftar Membership</a></li>
         <li><a href="{{ route('products.index') }}">Daftar Produk</a></li>
         <li><a href="{{ route('reviews.index') }}">Daftar Review</a></li>
+        <li><a href="{{ route('transactions.admin_index') }}">Daftar Transaksi</a></li>
         <li><a href="{{ route('vouchers.index') }}">Daftar Voucher</a></li>
-        <li><a href="{{ route('memberships.index') }}">Daftar Membership</a></li>
-        <li><a href="{{ route('news.index') }}">Daftar Berita</a></li>
     </ul>
 </body>
 </html>
