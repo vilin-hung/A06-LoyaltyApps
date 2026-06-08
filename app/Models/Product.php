2d01c98b2193f0fe71d4c357610a4d89dfdc0c78
vioshellina <agitaputri05@gmail.com>
2026-06-08 19:24:20 +0700

    feat(redeem,favorite): clean redeem and favorite implementation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,15 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [
-    'name',
-    'description',
-    'price',
-    'stock',
-    'category'
+        'name',
+        'description',
+        'price',
+        'stock',
+        'category'
     ];
 
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
     }
 }
