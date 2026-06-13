d591b253fd03044f0efd9e10d7e6d57b1966f723
Charly-437 <hicharly07@gmail.com>
2026-06-13 16:26:50 +0700

    refactor(news): modularize seeder and update views

--- resources/views/news/edit.blade.php
+++ resources/views/news/edit.blade.php
@@ -3,16 +3,10 @@
 <head>
     <title>Edit Berita</title>
     <style>
-        .btn {
-            padding: 5px 10px;
-            text-decoration: none;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            background-color: #f2f2f2;
-            color: black;
-            font-size: 14px;
-            cursor: pointer;
-        }
+        body { margin: 20px; color: #333; }
+        .btn { padding: 8px 12px; border: none; border-radius: 6px; cursor: pointer; text-decoration: none; display: inline-block; font-size: 14px; }
+        .btn_blue { background: #4545a5; color: #fff6fd; }
+        .btn_green { background: #7b9e87; color: #fff6fd; }
     </style>
 </head>
 <body>
@@ -34,9 +28,9 @@
             <label>Konten:</label><br>
             <textarea name="content" required>{{ $news->content }}</textarea>
         </p>
-        <button type="submit" class="btn">Update</button>
-    </form>
-    <br>
-    <a href="{{ route('news.index') }}">Kembali ke Daftar Berita</a>
+        <button type="submit" class="btn btn_green">Update</button>
+    </form> <br>
+    
+    <a href="{{ route('news.index') }}" class="btn btn_blue">Kembali ke Daftar Berita</a>
 </body>
 </html>
